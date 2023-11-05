3727fe114ce5513d0f54ce883f4ce1c1cadd0e19
sbx_user1051 <sbx_user1051@169.254.245.61>
2023-11-05 18:32:39 +0000

    Sun Nov 05 18:32:39 UTC 2023

---
+++ test/solutions/CHK/test-checkout.js
@@ -0,0 +1,17 @@
+var mocha = require('mocha')
+var describe = mocha.describe
+var it = mocha.it
+var assert = require('assert');
+const checkout = require('../../../lib/solutions/CHK/checkout');
+
+describe('CHK challenge: checkout', () => {
+
+    it('calls something',() => {
+        const testInput = `hiya
+        `
+
+
+        checkout(testInput)
+    })
+
+})
