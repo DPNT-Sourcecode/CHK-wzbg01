0323c5a0678fd7662c18909062b6d8467459c08c
sbx_user1051 <sbx_user1051@169.254.245.61>
2023-11-05 17:38:28 +0000

    Sun Nov 05 17:38:28 UTC 2023

--- test/solutions/SUM/test-sum.js
+++ test/solutions/SUM/test-sum.js
@@ -8,4 +8,15 @@ describe('SUM challenge: adding two numbers', function() {
 	it('should return 3, which is the sum of 1 and 2', function() {
 	    assert.equal(sum(1, 2), 3);
 	});
+
+
+	// //cover as much base as possible. without having to go through every cumbersome permutation
+	// it('should throw error if param[0] is not a positive integer', function(){
+	// })
+
+	it('should throw error if param[1] is not an integer between 0-100', () =>{
+		assert(1+1==2)
+	})
+
+
 });
