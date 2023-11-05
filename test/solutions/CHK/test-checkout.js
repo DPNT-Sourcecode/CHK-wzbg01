7d7c85015a5eaae85270097c0a5230d6ee706fb0
sbx_user1051 <sbx_user1051@169.254.245.61>
2023-11-05 19:47:27 +0000

    Sun Nov 05 19:47:27 UTC 2023

--- test/solutions/CHK/test-checkout.js
+++ test/solutions/CHK/test-checkout.js
@@ -5,13 +5,14 @@ var assert = require('assert');
 const checkout = require('../../../lib/solutions/CHK/checkout');
 
 describe('CHK challenge: checkout', () => {
-
-    //will write tests after first round of feedback in case i need to change requirements
+    //valid: ""
+    //invalid: ABCa,-,a,AxA
 
     it('calls something',() => {
-        const testInput = `3A2B1C1D`
+        const testInput = ``
         checkout(testInput)
     })
 
 })
+
 
