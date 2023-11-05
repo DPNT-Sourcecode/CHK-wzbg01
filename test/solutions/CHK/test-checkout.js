b40f508d43260f4020126f88bf0190dca913d78a
sbx_user1051 <sbx_user1051@169.254.245.61>
2023-11-05 19:33:42 +0000

    Sun Nov 05 19:33:42 UTC 2023

--- test/solutions/CHK/test-checkout.js
+++ test/solutions/CHK/test-checkout.js
@@ -6,9 +6,12 @@ const checkout = require('../../../lib/solutions/CHK/checkout');
 
 describe('CHK challenge: checkout', () => {
 
+    //will write tests after first round of feedback in case i need to change requirements
+
     it('calls something',() => {
         const testInput = `3A2B1C1D`
         checkout(testInput)
     })
 
 })
+
